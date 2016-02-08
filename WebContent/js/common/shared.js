50f356b1ec5ebda988d471a8e48d49f4ceecf6da
Luca Florido <lucaflorido@gmail.com>
2016-02-08 12:29:41 +0100

    all

--- WebContent/js/common/shared.js
+++ WebContent/js/common/shared.js
@@ -221,4 +221,60 @@ angular.module('modules.common.shared', [])
 	}
 	return factory;
 	
+}).factory('ModalFactory', function ($modal) {
+	var factory = {};
+	factory.sendMail = function(user,mailconfig,service){
+		var modalInstance = $modal.open({
+			  templateUrl: 'template/popup/sendemail.htm',
+			  controller: 'EmailCtrl',
+			  windowClass: 'app-modal-window',
+			  size: "lg",
+			  resolve: {
+				emailSender: function(){
+					return { user: user,mailconfig: mailconfig,service: service,type:'test' };
+				}
+			}
+		});
+	};
+	factory.sendDocument = function(user,document,service){
+		var modalInstance = $modal.open({
+			  templateUrl: 'template/popup/sendemail.htm',
+			  controller: 'EmailCtrl',
+			  windowClass: 'app-modal-window',
+			  size: "lg",
+			  resolve: {
+				emailSender: function(){
+					return { user: user,service: service,type:'document',document: document };
+				}
+			}
+		});
+	};
+	factory.confirm = function(){
+		var modalInstance = $modal.open({
+			  templateUrl: 'template/popup/confirm.htm',
+			  controller: 'ErrorCtrl',
+			  windowClass: 'app-modal-window',
+			  size: "lg",
+			  resolve: {
+				emailSender: function(){
+					return { };
+				}
+			  }
+		});
+	};
+	factory.error = function(message){
+		var modalInstance = $modal.open({
+			  templateUrl: 'template/popup/error.htm',
+			  controller: 'ErrorCtrl',
+			  windowClass: 'app-modal-window',
+			  size: "lg",
+			  resolve: {
+				emailSender: function(){
+					return { message: message };
+				}
+			}
+		});
+	};
+	return factory;
+	
 });
